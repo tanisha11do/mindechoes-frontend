f53e24c60bc4b498581eb165b6ba9465a58aec0b
Tanisha Pattnaik <tanishakhuc@gmail.com>
2024-02-16 01:58:20 +0530

    added 2 sections

--- mindechoes/src/App.jsx
+++ mindechoes/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import image1 from './assets/Section1.png';
-
+import image2 from './assets/Section2.png';
 
 function App() {
   return (
@@ -15,6 +15,13 @@ function App() {
         
         <img src={image1} alt='' className='section1img'/>
         <div><p>"You are the sky. Everything else is just the weather." - Pema Chödrön</p></div>
+      </section>
+      <section className='section2'>
+        <div>
+          <h1>Why take a Mental Health Test?</h1>
+          <h3>Mental health tests offer valuable insights into your emotional well-being. It's essential to remember that these tests are not diagnostic tools. They are meant to guide you and provide an indication of whether you might benefit from further evaluation by a mental health professional.</h3>
+        </div>
+        <img src={image2} alt="" className='section2img'/>
       </section>
     </div>
   );
